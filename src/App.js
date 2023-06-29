4fce3ecb664a4e82cb68e9a4c88462719c3a91c7
Mael7ax <mael7ax@gmail.com>
2023-06-29 19:16:17 +0600

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+
+
+function App(){
+    return (
+      <>
+      <Header/>
+      <h1>hello world</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum natus iure nulla rerum, quas consequatur.</p>
+     
+      </>
+    )
+  }
+  export default App;
+  
